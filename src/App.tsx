ac01d6398cd4ab4cb9f7efa09c7b602e2c56f9cf
dongheeYoo <yoodhajou0616@gmail.com>
2024-03-27 18:26:32 +0900

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div({
+  display: 'flex',
+});
+
+function App() {
+  return <Container>Start</Container>;
+}
+
+export default App;
