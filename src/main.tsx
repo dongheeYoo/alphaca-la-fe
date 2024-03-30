5da28ae41b88cec8a694f47846055a9176c6a7df
dongheeYoo <yoodhajou0616@gmail.com>
2024-03-30 21:47:46 +0900

    yarn react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/index';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
