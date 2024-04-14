752c15e58d2bc6ec64d273bc6602ebbc733412fd
dongheeYoo <yoodhajou0616@gmail.com>
2024-04-15 01:11:27 +0900

    set up theme

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,38 +1,85 @@
 import { Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { MainPage } from '../MainPage';
 import { Navbar } from '../Navbar';
 import { CharacterListView } from '../CharacterListView';
 import { GroupsListView } from '../GroupsListView';
 import { CalenderView } from '../CalenderView';
+import { dark, light } from '../../style/theme';
+import { useTheme } from '../../hooks/useTheme';
 
-const Container = styled.div({
-  width: '100%',
-  height: '100%',
-});
-const BodySection = styled.div({
-  display: 'flex',
-  position: 'relative',
-  top: '50px',
-  width: '100%',
-  overflow: 'hidden',
-  justifyContent: 'center',
-});
+// const Container = styled.div({
+//   width: '100%',
+//   height: '100%',
+//   backgroundColor:
+//   background-color: ${props => props.theme.colors.bgColor};
+// });
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${props => props.theme.colors.bgColor};
+`;
+
+//.css-dev-only-do-not-override-42nv3w).ant-table-wrapper .ant-table
+
+// const BodySection = styled.div((props: any) => ({
+//   display: 'flex',
+//   position: 'relative',
+//   top: '50px',
+//   width: '100%',
+//   overflow: 'hidden',
+//   justifyContent: 'center',
+//   flexDirection: 'column',
+//   '.ant-table': {
+//     backgroundColor: 'black',
+//     color: 'white',
+//   },
+//   '.ant-table-thead>tr>th': {
+//     backgroundColor: 'black',
+//     color: 'white',
+//   },
+// }));
+
+const BodySection = styled.div`
+  display: flex;
+  position: relative;
+  top: 50px;
+  width: 100%;
+  overflow: hidden;
+  justify-content: center;
+  //flex-direction: column; 중앙정렬?
+
+  .ant-table {
+    background-color: ${props => props.theme.colors.tableColor};
+    color: ${props => props.theme.colors.fontColor};
+  }
+
+  .ant-table-thead > tr > th {
+    background-color: ${props => props.theme.colors.tableColor};
+    color: ${props => props.theme.colors.fontColor};
+  }
+`;
 
 function App() {
+  const [themeMode, toggleTheme] = useTheme(); // hook 함수 하용
+  const theme = themeMode === 'light' ? light : dark; // 테마 환경에 맞는 테마 컬러 가져오기.
+
   return (
-    <Container>
-      {/* TODO: Navbar */}
-      <Navbar />
-      <BodySection>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/characters" element={<CharacterListView />} />
-          <Route path="/groups" element={<GroupsListView />} />
-          <Route path="/calender" element={<CalenderView />} />
-        </Routes>
-      </BodySection>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        {/* TODO: Navbar */}
+        <Navbar themeMode={themeMode} toggleTheme={toggleTheme} />
+        <BodySection>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/characters" element={<CharacterListView />} />
+            <Route path="/groups" element={<GroupsListView />} />
+            <Route path="/calender" element={<CalenderView />} />
+          </Routes>
+        </BodySection>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/style/theme.ts
@@ -0,0 +1,15 @@
+export const dark = {
+  colors: {
+    bgColor: '#202124',
+    tableColor: 'black',
+    fontColor: 'white',
+  },
+};
+
+export const light = {
+  colors: {
+    bgColor: '#FEFEFE',
+    tableColor: 'white',
+    fontColor: 'black',
+  },
+};
