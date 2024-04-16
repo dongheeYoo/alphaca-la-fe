18e4a3f3b580cd27716182afed58e663abcd931f
dongheeYoo <yoodhajou0616@gmail.com>
2024-04-17 01:49:07 +0900

    set up server endpoint (#22)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,8 @@ import { Character, Group } from './types';
 const APIKEY =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAzMzQ1NzkifQ.bQ656vskR9DwbkCgGlEmE-MH3tUSQVkkod7aXPwb50N29Vb25XdKaNPbS_r97dnwkqfCJvMpHQxp_mK_UQLdB-bGytVaT8t6DvnkuL_jExMdVtlHVArEuEWnsXc89Tlu0KKisJD-21GMVGxKLY0eEcZQY_0iCOFdpHV5UJ86SDLV_ixuwyoU28rP4t-wnd8hKzphuFlZnP3m5Fw6V00mjzDcDONj4PTEpBKFvfafzE8RV0LzuE_7Oa2h3IYcnGLU29fMBne9OKz5AMvFVdA_lkhN7move2pSPR_Oh4NEReeB1nuWSb-VeVeBn6aihP9EX5NQsvFQXKzDeeXHnEtMyw';
 
-const END_POINT = `http://localhost:8080/api`;
+//const END_POINT = `http://localhost:8080/api`;
+const END_POINT = `http://115.85.180.49:8080/api`;
 
 class API {
   async getCharacterInfo(characterName: string) {
